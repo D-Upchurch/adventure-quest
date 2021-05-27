e878117d396d4ffbb8e535dbbeaf1916b50b7b8b
Dakota <dakota.c.upchurch@gmail.com>
2021-05-27 14:50:36 -0500

    phase 3 of adventure quest complete

--- Program.cs
+++ Program.cs
@@ -80,6 +80,39 @@ namespace adventure_quest
             {
                 Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
             }
+
+            Console.WriteLine("Would you like to play again? y/n");
+            string answer = Console.ReadLine();
+            while (answer == "y")
+            {
+                // Reset the adventurer's Awesomness to 50.
+                theAdventurer.Awesomeness = 50;
+
+                // Loop through all the challenges and subject the Adventurer to them
+                foreach (Challenge challenge in challenges)
+                {
+                    challenge.RunChallenge(theAdventurer);
+                }
+
+                // This code examines how Awesome the Adventurer is after completing the challenges
+                // And praises or humiliates them accordingly
+                if (theAdventurer.Awesomeness >= maxAwesomeness)
+                {
+                    Console.WriteLine("YOU DID IT! You are truly awesome!");
+                }
+                else if (theAdventurer.Awesomeness <= minAwesomeness)
+                {
+                    Console.WriteLine("Get out of my sight. Your lack of awesomeness offends me!");
+                }
+                else
+                {
+                    Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
+                }
+
+                Console.WriteLine("Would you like to continue? ");
+                answer = Console.ReadLine();
+            }
+
         }
     }
 }
